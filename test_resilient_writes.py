8d17083a855eb6761d055510362148e00463ab0a
logan <logan.sae.lee1@gmail.com>
2025-09-16 14:06:40 -0700

    delete initial tests

--- test_resilient_writes.py
+++
@@ -1,3 +0,0 @@
-def test_resilient_write():
-    # Placeholder: you'd simulate an error and retry logic
-    assert True
--- test_shutdown.py
+++
@@ -1,3 +0,0 @@
-def test_shutdown_behavior():
-    # Placeholder: in real test, you'd start test_pipeline.py in subprocess
-    assert True  # pretend system exits cleanly
